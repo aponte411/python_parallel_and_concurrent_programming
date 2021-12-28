12b742ed5c41b66d3f126502649fccf7b8d71e6e
David Aponte <davidaponte@system76-pc.localdomain>
2021-12-28 14:11:54 -0500

    fix: update process example

--- synchronization/producer_consumer_processes.py
+++ synchronization/producer_consumer_processes.py
@@ -9,25 +9,26 @@ def cpu_work(work_units: int):
         x += 1
 
 
-def soup_producer():
+def soup_producer(serving_line: mp.Queue):
     for soup in range(20):
         serving_line.put_nowait(f"Soup #{soup}")
-        print(f"Served bowl {soup}. Remaining capacity: {serving_line.maxsize - serving_line.qsize()}")
+        print(f"Served bowl {soup}. Remaining capacity: {serving_line._maxsize - serving_line.qsize()}")
         # Simulating the CPU not doing work, maybe the consumer is doing some I/O work
         time.sleep(0.2)
+    serving_line.put_nowait("#")
+    serving_line.put_nowait("#")
 
 
-def soup_consumer():
+def soup_consumer(serving_line: mp.Queue):
     while True:
         bowl = serving_line.get()
+        if bowl == "#": break
         print(f"Ate {bowl}")
         cpu_work(4)
-        serving_line.task_done()
 
 if __name__ == "__main__":
     # Create more consumers/workers than producers
     for consumer in range(2):
-        mp.Process(target=soup_consumer,args=(serving_line,),daemon=True).start()
+        mp.Process(target=soup_consumer,args=(serving_line,)).start()
     mp.Process(target=soup_producer,args=(serving_line,)).start()
-    serving_line.join()
 
