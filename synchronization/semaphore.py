bf19ff83eac8e8230ee0a818816b58f2e234ff86
David Aponte <davidaponte@system76-pc.localdomain>
2021-12-28 14:38:54 -0500

    chore: adding comments

--- synchronization/semaphore.py
+++ synchronization/semaphore.py
@@ -2,14 +2,22 @@ import random
 import threading
 import time
 
+# If the size of the resource is bounded, use a BoundedSemaphore()
 charger = threading.Semaphore(4)
 
 def cellphone():
     name = threading.current_thread().getName()
     with charger:
         print(f"{name} is charging their phone")
+        # Some I/O bound work like connect to db and querying it
+        # conn = connectdb()
+        # try:
+        #   use conn
+        # finally:
+        #   conn.close()
         time.sleep(random.uniform(1,2))
     print("Done!")
+
 
 if __name__ == "__main__":
     for phone in range(10):
