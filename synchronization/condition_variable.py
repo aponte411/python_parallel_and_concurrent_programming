837d743b057ca773ac6be5200fcf088e4a565a7a
David Aponte <davidaponte@system76-pc.localdomain>
2021-12-28 11:39:08 -0500

    initial commit

---
+++ synchronization/condition_variable.py
@@ -0,0 +1,23 @@
+import threading
+
+soup_lid = threading.Lock()
+soup_servings = 500
+soup_taken = threading.Condition(lock=soup_lid)
+
+def hungry_philosophers(person_id: int) -> None:
+    global soup_servings
+    while soup_servings > 0:
+        with soup_lid:
+            while (person_id!=(soup_servings%2)) and (soup_servings>0):
+                print(f"Person {person_id} checked..and then put the lid back")
+                soup_taken.wait()
+            if (soup_servings > 0):
+                soup_servings -= 1
+                print(f"Person {person_id} took soup! Servings left {soup_servings}")
+                soup_taken.notify() # if there are more than 2 threads, use .notify_all()
+
+
+if __name__ == "__main__":
+    for person in range(2):
+        threading.Thread(target=hungry_philosophers,args=(person,)).start()
+
